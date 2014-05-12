ad5b2651d6f435e162e9efcde5b7dd19c13900a3
Marcus Nyeholt <nyeholt@gmail.com>
2014-05-12 12:56:32 +1000

    Added date of notice creation to message displayed in the CMS

--- javascript/timednotices-cms.js
+++ javascript/timednotices-cms.js
@@ -17,11 +17,21 @@
 							.addClass(this.MessageType)
 							.attr('data-id', this.ID)
 							.html(this.Message)
-						
+
+                        var year = this.Created.slice(0,4);
+                        var month = this.Created.slice(5,7);
+                        var day = this.Created.slice(8,10);
+                        var time = this.Created.slice(11,16);
+
+                        var dateStr = day + '/' + month + '/' + year + ' at ' + time;
+
+                        var notice_time = $('<p>' + dateStr + '</p>')
+                            .addClass('notice-time')
 						var snoozer = $('<div>Snooze for <a href="#" rel="15">15 mins</a>, <a href="#" rel="60">1 hour</a>, <a href="#" rel="1440">1 day</a></div>')
 							.addClass('notice-snoozer')
-						
-						entry.append(snoozer);
+
+                        entry.append(snoozer);
+						entry.append(notice_time);
 						container.append(entry);
 					
 					});
@@ -52,6 +62,20 @@
 				updateNotices();
 			}
 		});
+		
+		$('#timed-notices .message a').entwine({
+			onclick: function (e) {
+				// handle using CMS request instead of directly linking
+				var href = $(this).attr('href');
+				if (href.indexOf('admin/') >= 0) {
+					href = $('base').attr('href') + href;
+					var cmsCont = $('.cms-container');
+					if(!cmsCont.entwine('.ss').loadPanel(href)) {
+						return false;
+					}
+				}
+			}
+		})
 		
 		$('#timed-notices .notice-snoozer a').entwine({
 			onclick: function (e) {
